8bb748d719b66327d1d95e4979338cbd881f6ca7
Dragon Black <mdbetico.96@gmail.com>
2022-12-08 13:20:59 +0000

    continuacion fase 4

---
+++ src/main/java/dad/codesignal/ExploringTheWater.java
@@ -0,0 +1,12 @@
+package dad.codesignal;
+
+public class ExploringTheWater {
+
+	
+	public int[] alternatingSums(int[] a) {
+		// TODO Auto-generated method stub
+
+		return null;
+	}
+	
+}
---
+++ src/test/java/dad/codesignal04/AlternatingSums.java
@@ -0,0 +1,19 @@
+package dad.codesignal04;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AlternatingSums {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
